aff102723c0bdcea27f6e16e2ea31c0fee921683
Juri Timo?in <draco.ater@gmail.com>
2018-01-20 11:15:27 +0200

    Use curl instead of wget to download slave.jar

--- test/integration/bootstrap.sh
+++ test/integration/bootstrap.sh
@@ -4,7 +4,7 @@ echo "Install Jenkins"
 
 echo "deb http://pkg.jenkins.io/debian-stable binary/" > /etc/apt/sources.list.d/jenkins.list
 apt-get update
-apt-get install jenkins -y --allow-unauthenticated
+apt-get install curl jenkins -y --allow-unauthenticated
 
 echo -n "Wait for jenkins to start and create admin user."
 for i in {1..10} ; do
@@ -15,6 +15,10 @@ for i in {1..10} ; do
 	fi
 	echo -n "."
 done
+
+echo "Download slave.jar"
+#wget -t3 --retry-on-http-error 503 -O /tmp/slave.jar http://localhost:8080/jnlpJars/slave.jar
+curl --retry 5 --retry-delay 0 http://localhost:8080/jnlpJars/slave.jar > /tmp/slave.jar
 
 echo "Disable setup wizard in Jenkins"
 cp /var/lib/jenkins/{jenkins.install.UpgradeWizard.state,jenkins.install.InstallUtil.lastExecVersion}
